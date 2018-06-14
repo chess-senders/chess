0aa15e5eabab254c711641a93effa1b8f83f58ae
Davey Wright <daveywright@Daveys-MBP.home>
2018-06-14 12:40:52 +0100

    refactor: user omniauth config to player

--- app/controllers/players/omniauth_callbacks_controller.rb
+++ app/controllers/players/omniauth_callbacks_controller.rb
@@ -1,14 +1,14 @@
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+class Players::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    @player = Player.from_omniauth(request.env["omniauth.auth"])
 
-    if @user.persisted?
-      sign_in_and_redirect @user
+    if @player.persisted?
+      sign_in_and_redirect @player
       set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      redirect_to new_player_registration_url
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :players, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  devise_for :users
+  devise_for :players, controllers: { omniauth_callbacks: 'players/omniauth_callbacks' }
   root 'static_pages#index'
 end
