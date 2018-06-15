69b33bf06b8c4b4b4ec24e552d61b51a1465e961
Davey Wright <daveywright@Daveys-MBP.home>
2018-06-15 09:37:58 +0100

    feat: add devise views and add playername to player table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180614113248) do
+ActiveRecord::Schema.define(version: 20180615082942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,9 @@ ActiveRecord::Schema.define(version: 20180614113248) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "playername"
     t.index ["email"], name: "index_players_on_email", unique: true, using: :btree
+    t.index ["playername"], name: "index_players_on_playername", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_players_on_reset_password_token", unique: true, using: :btree
   end
 
