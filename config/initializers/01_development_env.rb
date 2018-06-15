738860a539c61370236833b62535bcc575db93db
Davey Wright <daveywright@Daveys-MBP.home>
2018-06-15 22:30:58 +0100

    doc: add figaro gem and move env variables

--- config/initializers/01_development_env.rb
+++
@@ -1,7 +0,0 @@
-if Rails.env.development?
-  ENV['FACEBOOK_APP_ID'] = "201381033822383"
-  ENV['FACEBOOK_APP_SECRET'] = "ffe5f4abbe8f534020bf2c346bd740b2"
-
-  ENV['GOOGLE_CLIENT_ID'] = "1039777499772-31jekumgjo9ndmonhtvvga73kucah8k1.apps.googleusercontent.com"
-  ENV['GOOGLE_CLIENT_SECRET'] = "Zw8YqXfX3wJSfWIZhFqiFvXh"
-end
