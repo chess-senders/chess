b26ba7673161393215aaee2205d2a39d02542a0a
Davey Wright <daveywright@Daveys-MBP.home>
2018-06-15 15:36:33 +0100

    refactor: change playername unique to false in db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180615082942) do
+ActiveRecord::Schema.define(version: 20180615143138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20180615082942) do
     t.string   "uid"
     t.string   "playername"
     t.index ["email"], name: "index_players_on_email", unique: true, using: :btree
-    t.index ["playername"], name: "index_players_on_playername", unique: true, using: :btree
+    t.index ["playername"], name: "index_players_on_playername", using: :btree
     t.index ["reset_password_token"], name: "index_players_on_reset_password_token", unique: true, using: :btree
   end
 
