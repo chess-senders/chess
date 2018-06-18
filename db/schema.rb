38ee62dc54b6fe90ae749675e678034229ff660f
Davey Wright <daveywright@Daveys-MBP.home>
2018-06-18 14:32:39 +0100

    db: change game state default to waiting for players

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180616232240) do
+ActiveRecord::Schema.define(version: 20180618132813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 20180616232240) do
   create_table "games", force: :cascade do |t|
     t.integer  "white_player_id"
     t.integer  "black_player_id"
-    t.integer  "state"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.integer  "state",           default: 0
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.index ["white_player_id", "black_player_id"], name: "index_games_on_white_player_id_and_black_player_id", using: :btree
   end
 
