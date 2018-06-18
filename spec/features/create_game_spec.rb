3ab4f7de6ae369d6a43f04489a69110b1f7ee20e
Davey Wright <daveywright@Daveys-MBP.home>
2018-06-18 22:32:17 +0100

    test: refactor create game spec

--- spec/features/create_game_spec.rb
+++ spec/features/create_game_spec.rb
@@ -1,16 +1,13 @@
 require 'rails_helper'
+require 'support/sign_in'
 
 feature 'A player creates a game' do
 
   scenario "A player creates a game with a valid user" do
-    player1 = FactoryBot.create(:player, playername: 'Wayne')
-    login_as(player1, :scope => :player)
-    visit "/players/#{player1.id}"
-    expect(page).to have_content(player1.playername)
-    expect(page).to have_button('create game')
+    player = FactoryBot.create(:player, playername: 'Wayne')
+    game = FactoryBot.create(:game, white_player: player)
 
-    # user creates game
-    game = FactoryBot.create(:game, white_player: player1)
+    sign_in(player)
     click_button('create game')
     visit '/games/new'
     fill_in('Game name', with: game.name)
---
+++ spec/support/sign_in.rb
@@ -0,0 +1,6 @@
+def sign_in(player)
+  login_as(player, :scope => :player)
+  visit "/players/#{player.id}"
+  expect(page).to have_content(player.playername)
+  expect(page).to have_button('create game')
+end
