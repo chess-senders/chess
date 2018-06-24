eb186e479337d2319268c7800501f4718fac9174
KD33 <Walshk94@gmail.com>
2018-06-24 02:00:50 +0000

    updated piece movement and validation

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -3,43 +3,116 @@ class Piece < ApplicationRecord
 
   belongs_to :game
 
-  def move_linear(new_row)
-    direction = new_row - self.row
-    (direction.abs).times do
-      if direction < 0
-        self.row -= 1
-      else
-        self.row += 1
-      end
+  #don't update state if 0 is passed in to a move
+  def move_linear(rows_to_move)
+      self.row += rows_to_move
+  end
+
+  def move_lateral(cols_to_move)
+    self.column += cols_to_move
+  end
+
+  def move_diagonal(rows_to_move, cols_to_move)
+    move_linear(rows_to_move)
+    move_lateral(cols_to_move)
+  end
+
+  def validate_row_movement(new_row)
+    if(new_row - self.row > 8) || (new_row - self.row < 1)
+      return 0
     end
   end
 
-  def move_lateral(new_column)
-    direction = new_column - self.column
-    (direction.abs).times do
-      if direction < 0
-        self.column -= 1
-      else
-        self.column +=1
-      end
+  def validate_col_movement(new_col)
+    if (new_col - self.col > 8) || (new_col - self.col < 1)
+      return 0
+    end
+  end
+end
+
+
+class King < Piece
+  def validate_move(new_row, new_col)
+    validate_row_movement(new_row)
+    validate_col_movement(new_col)
+
+    if (new_row - self.row > 1) || (new_row - self.row < -1)
+      #invalid move
+      return 0
+    end
+
+    if (new_col - self.col > 1) || (new_col - self.col < -1)
+      return 0
     end
   end
 
-  def move_diagonal(new_column, new_row)
-    move_linear(new_row)
-    move_lateral(new_column)
+  def validate_col_movement(new_col)
+    if (new_col - self.col > 1) || (new_col - self.col < -1)
+      #invalid move
+      return 0
+    else
+      return (new_col - self.col)
+    end
+
+end
+end
+
+class Queen < Piece
+
+end
+
+class Rook < Piece
+  #make sure cannot move diagonally
+  def validate_move(new_row, new_col)
+    validate_col_movement(new_col)
+    validate_row_movement(new_row)
+    if (new_row != self.row) && (new_col != self.col)
+      return 0
+    end
+  end
+
+end
+
+class Bishop < Piece
+  #must move diagonally
+  validate_col_movement(new_col)
+  validate_row_movement(new_row)
+  def validate_move(new_row, new_col)
+    if (new_row == self.row) || (new_col == self.col)
+      return 0
+    end
+  end
+end
+
+class Knight < Piece
+  def validate_move(new_row, new_col)
+    validate_col_movement(new_col)
+    validate_row_movement(new_row)
+    if (new_row - self.row > 2) || (new_row - self.row < -2)
+      return 0
+    end
+
+    if (new_col - self.col > 2) || (new_col - self.col < -2)
+      return 0
+    end
+
+    if (new_row - self.row == 2 || new_row - self.row == -2) && (new_col - self.col != 1 || new_col - self.col != -1)
+      return 0
+    end
+
+    if (new_col - self.col == 2 || new_col - self.col == -2) && (new_row - self.col != 1 || new_row - self.col != -1)
+      return 0
+    end
   end
 end
 
 class Pawn < Piece
-  
-end
-
-class Rook < Piece
-end
-
-class King < Piece
-end
-
-class Queen < Piece
+  def validate_move(new_row, new_col)
+    validate_row_movement(new_row)
+    return 0 if new_col != self.col
+    if self.row == 2
+      return 0 if (new_row - self.row > 2 )
+    else
+      return 0 if (new_row - self.row != 1)
+  end
 end
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -9,28 +9,28 @@ RSpec.describe Piece, type: :model do
 
     it "can move linearly in either direction" do
       piece = FactoryBot.build(:piece)
-      piece.move_linear(5)
+      piece.move_linear(3)
       expect(piece.row).to eq(5)
-      piece.move_linear(2)
-      expect(piece.row).to eq(2)
+      piece.move_linear(-2)
+      expect(piece.row).to eq(3)
     end
 
     it "can move laterally in either direction" do
       piece = FactoryBot.build(:piece)
-      piece.move_lateral(8)
-      expect(piece.column).to eq(8)
-      piece.move_lateral(2)
+      piece.move_lateral(3)
+      expect(piece.column).to eq(4)
+      piece.move_lateral(-2)
       expect(piece.column).to eq(2)
     end
 
     it "can move diagonally in either direction" do
       piece = FactoryBot.build(:piece)
       piece.move_diagonal(4,4)
+      expect(piece.row).to eq(6)
+      expect(piece.column).to eq(5)
+      piece.move_diagonal(-2,-2)
       expect(piece.row).to eq(4)
-      expect(piece.column).to eq(4)
-      piece.move_diagonal(2,2)
-      expect(piece.row).to eq(2)
-      expect(piece.column).to eq(2)
+      expect(piece.column).to eq(3)
     end
   end
 end
