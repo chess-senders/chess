fb1ab203eb2cfbc5c3e6c39065500979c219e5f2
Davey Wright <daveywright@Daveys-MBP.home>
2018-06-29 11:47:23 +0100

    feat: add move_to method to piece model

--- app/services/pieces/is_obstructed.rb
+++ app/services/pieces/is_obstructed.rb
@@ -2,56 +2,56 @@ module Pieces
   class IsObstructed < ApplicationService
     def initialize(move, game)
       @game = game
-      @start_column = move[:start][:column]
-      @start_row = move[:start][:row]
-      @end_column = move[:end][:column]
-      @end_row = move[:end][:row]
+      @current_column = move[:current][:column]
+      @current_row = move[:current][:row]
+      @new_column = move[:new][:column]
+      @new_row = move[:new][:row]
     end
 
     def call
-      return horizontal_obstruction if start_row == end_row
-      return vertical_obstruction if start_column == end_column
-      return diagnol_obstruction if (start_column - end_column).abs == (start_row - end_row).abs
+      return horizontal_obstruction if current_row == new_row
+      return vertical_obstruction if current_column == new_column
+      return diagnol_obstruction if (current_column - new_column).abs == (current_row - new_row).abs
       return nil
     end
 
     private
 
-    attr_accessor :game, :start_row, :start_column, :end_row, :end_column
+    attr_accessor :game, :current_row, :current_column, :new_row, :new_column
 
-    def column_start
-      [start_column, end_column].min + 1
+    def column_current
+      [current_column, new_column].min + 1
     end
 
-    def row_start
-      [start_row, end_row].min + 1
+    def row_current
+      [current_row, new_row].min + 1
     end
 
-    def column_end
-      [start_column, end_column].max
+    def column_new
+      [current_column, new_column].max
     end
 
-    def row_end
-      [start_row, end_row].max + 1
+    def row_new
+      [current_row, new_row].max + 1
     end
 
     def horizontal_obstruction
-      (column_start...column_end).each do |column|
-        return true if game.square_occupied?(column, start_row)
+      (column_current...column_new).each do |column|
+        return true if game.square_occupied?(column, current_row)
       end
       false
     end
 
     def vertical_obstruction
-      (row_start...row_end).each do |row|
-        return true if game.square_occupied?(start_column, row)
+      (row_current...row_new).each do |row|
+        return true if game.square_occupied?(current_column, row)
       end
       false
     end
 
     def diagnol_obstruction
-      (row_start...row_end).each do |row|
-        (column_start...column_end).each do |column|
+      (row_current...row_new).each do |row|
+        (column_current...column_new).each do |column|
           return true if game.square_occupied?(column, row)
         end
       end
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe Piece, type: :model do
   player = FactoryBot.create(:player, playername: 'Wayne')
   game = FactoryBot.create(:game, white_player: player)
-  FactoryBot.create(:piece, game: game, row: 2, column: 3)
-  FactoryBot.create(:piece, game: game, row: 2, column: 2)
-  FactoryBot.create(:piece, game: game, row: 3, column: 2)
-  FactoryBot.create(:piece, game: game, row: 3, column: 3)
+  piece1 = FactoryBot.create(:piece, game: game, row: 2, column: 3, player_color: 1)
+  piece2 = FactoryBot.create(:piece, game: game, row: 1, column: 2, player_color: 0)
+  piece3 = FactoryBot.create(:piece, game: game, row: 3, column: 2, player_color: 1)
+  FactoryBot.create(:piece, game: game, row: 3, column: 3, player_color: 0)
 
   context 'attempting to move a piece' do
     it 'Should detect horizontal obstructions' do
       obstruction = Pieces::IsObstructed.call(
         {
-          start: { row: 2, column: 2 },
-          end: { row: 2, column: 4 }
+          current: { row: 2, column: 2 },
+          new: { row: 2, column: 4 }
         }, game
       )
       expect(obstruction).to eq(true)
@@ -22,8 +22,8 @@ RSpec.describe Piece, type: :model do
     it 'Should detect vertical obstructions' do
       obstruction = Pieces::IsObstructed.call(
         {
-          start: { row: 3, column: 2 },
-          end: { row: 0, column: 2 }
+          current: { row: 3, column: 2 },
+          new: { row: 0, column: 2 }
         }, game
       )
       expect(obstruction).to eq(true)
@@ -32,8 +32,8 @@ RSpec.describe Piece, type: :model do
     it 'Should detect diagnol obstructions' do
       obstruction = Pieces::IsObstructed.call(
         {
-          start: { row: 0, column: 0 },
-          end: { row: 3, column: 3 }
+          current: { row: 0, column: 0 },
+          new: { row: 3, column: 3 }
         }, game
       )
       expect(obstruction).to eq(true)
@@ -42,11 +42,37 @@ RSpec.describe Piece, type: :model do
     it 'Should detect an invalid input' do
       obstruction = Pieces::IsObstructed.call(
         {
-          start: { row: 0, column: 0 },
-          end: { row: 1, column: 3 }
+          current: { row: 0, column: 0 },
+          new: { row: 1, column: 3 }
         }, game
       )
       expect(obstruction).to eq(nil)
+    end
+  end
+
+  context 'Capturing a piece' do
+    it 'Should not move to new location if same colour piece is there' do
+      move = piece1.move_to!(piece3[:column], piece3[:row])
+      expect(move).to eq(false)
+      expect(piece1[:column]).to eq(3)
+      expect(piece1[:row]).to eq(2)
+      expect(piece3.captured).to eq(false)
+    end
+
+    it 'Should move to new location and capture opponents piece' do
+      move = piece1.move_to!(piece2[:column], piece2[:row])
+      expect(move).to eq(true)
+      expect(piece1[:column]).to eq(piece2[:column])
+      expect(piece1[:row]).to eq(piece2[:row])
+      piece2.reload
+      expect(piece2.captured).to eq(true)
+    end
+
+    it 'Should move to new location if no other piece is there' do
+      move = piece1.move_to!(1, 1)
+      expect(move).to eq(true)
+      expect(piece1[:column]).to eq(1)
+      expect(piece1[:row]).to eq(1)
     end
   end
 end
