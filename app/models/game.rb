c97f192e5fe52917d3d321f1a5ea219c01e62129
tallKitties <bkautt73@gmail.com>
2018-06-30 19:17:38 +0000

    adding pieces for both players in game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,6 @@
 class Game < ApplicationRecord
+  validates :white_player, presence: true
+
   belongs_to :white_player, class_name: :Player
   belongs_to :black_player, class_name: :Player, optional: true
   has_many :pieces
@@ -18,5 +20,88 @@ class Game < ApplicationRecord
 
   def square_occupied?(column, row)
     pieces.where(column: column, row: row).any?
+  end
+
+  def add_pieces_to_board
+    add_back_row(white_player_id, 0)
+    add_pawns(white_player_id, 1)
+
+    add_back_row(black_player_id, 7)
+    add_pawns(black_player_id, 6)
+  end
+
+  def add_back_row(player_id, row)
+    add_rook(player_id, row, 0)
+    add_rook(player_id, row, 7)
+
+    add_knight(player_id, row, 1)
+    add_knight(player_id, row, 6)
+
+    add_bishop(player_id, row, 2)
+    add_bishop(player_id, row, 5)
+
+    add_queen(player_id, row, 3)
+
+    add_king(player_id, row, 4)
+  end
+
+  def add_rook(player_id, row, col)
+    pieces << Piece.create(
+      player_color: player_id,
+      row: row,
+      column: col
+      # type: 2
+    )
+  end
+
+  def add_knight(player_id, row, col)
+    pieces << Piece.create(
+      player_color: player_id,
+      row: row,
+      column: col
+      # type: 4
+    )
+  end
+
+  def add_bishop(player_id, row, col)
+    pieces << Piece.create(
+      player_color: player_id,
+      row: row,
+      column: col
+      # type: 3
+    )
+  end
+
+  def add_queen(player_id, row, col)
+    pieces << Piece.create(
+      player_color: player_id,
+      row: row,
+      column: col
+      # type: 1
+    )
+  end
+
+  def add_king(player_id, row, col)
+    pieces << Piece.create(
+      player_color: player_id,
+      row: row,
+      column: col
+      # type: 0
+    )
+  end
+
+  def add_pawns(player_id, row)
+    8.times do |col|
+      add_pawn(player_id, row, col)
+    end
+  end
+
+  def add_pawn(player_id, row, col)
+    pieces << Piece.create(
+      player_color: player_id,
+      row: row,
+      column: col
+      # type: 5
+    )
   end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,4 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
+  describe 'validations' do
+    it 'is not valid without a white player' do
+      expect {
+        FactoryBot.create(:game, white_player: nil)
+      }.to raise_error(
+        ActiveRecord::RecordInvalid,
+        'Validation failed: White player can\'t be blank, White player must exist')
+    end
+  end
+
+  describe '#add_pieces_to_board' do
+    it 'should add 16 white and 16 black pieces to the board' do
+      white_player = FactoryBot.create(:player, playername: 'Wayne')
+      black_player = FactoryBot.create(:player, playername: 'John')
+      game = FactoryBot.create(
+        :game,
+        white_player: white_player,
+        black_player: black_player
+      )
+
+      game.add_pieces_to_board
+
+      white_pieces = game.pieces.where(player_color: game.white_player_id)
+      black_pieces = game.pieces.where(player_color: game.black_player_id)
+      expect(white_pieces.count).to eq(16)
+      expect(black_pieces.count).to eq(16)
+    end
+  end
+
 end
