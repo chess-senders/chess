e3e8b85a728c459de03f8aa45c13beb124e34b52
Davey Wright <daveywright@Daveys-MBP.home>
2018-06-30 23:02:40 +0100

    test: refactor join game spec

--- spec/features/join_game_spec.rb
+++ spec/features/join_game_spec.rb
@@ -6,13 +6,13 @@ require 'support/sign_in'
 feature 'A player tries to join a game' do
   white_player = FactoryBot.create(:player, playername: 'Rikter')
   black_player = FactoryBot.create(:player, playername: 'Sender')
-  game = FactoryBot.create(:game, white_player: white_player)
+  game = FactoryBot.create(:game, white_player: white_player, name: 'shaka')
 
   scenario 'player successfully joins game' do
     sign_in(black_player)
     visit '/games'
     expect(page).to have_link('Game ON!')
-    click_link 'Game ON!'
+    click_link 'shaka'
     visit "/games/#{game.id}"
     expect(Game.first.black_player.playername).to eq('Sender')
     expect(Game.first.state).to eq("White's Turn")
@@ -22,7 +22,7 @@ feature 'A player tries to join a game' do
     sign_in(black_player)
     visit '/games'
     expect(page).to have_link('Game ON!')
-    click_link 'Game ON!'
+    click_link 'shaka'
     expect { visit '/games/100' }.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
