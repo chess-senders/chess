c22e3ac1dda6285c78488ec986db7186e1cda4e3
tallKitties <bkautt73@gmail.com>
2018-06-30 19:37:37 +0000

    add pieces for 2nd player later if they don't exist on creation

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -23,11 +23,50 @@ RSpec.describe Game, type: :model do
 
       game.add_pieces_to_board
 
-      white_pieces = game.pieces.where(player_color: game.white_player_id)
-      black_pieces = game.pieces.where(player_color: game.black_player_id)
+      white_pieces = game.pieces.where(player_color: game.white_player)
+      black_pieces = game.pieces.where(player_color: game.black_player)
+      expect(white_pieces.count).to eq(16)
+      expect(black_pieces.count).to eq(16)
+    end
+
+    it "shouldn't add black pieces without a black_player in Game" do
+      white_player = FactoryBot.create(:player, playername: 'Wayne')
+      game = FactoryBot.create(
+        :game,
+        white_player: white_player
+      )
+
+      game.add_pieces_to_board
+
+      white_pieces = game.pieces.where(player_color: game.white_player)
+      black_pieces = game.pieces.where(player_color: game.black_player)
+      expect(white_pieces.count).to eq(16)
+      expect(black_pieces.count).to eq(0)
+    end
+
+    it "should only add black pieces when a 2nd player joins" do
+      white_player = FactoryBot.create(:player, playername: 'Wayne')
+      game = FactoryBot.create(
+        :game,
+        white_player: white_player
+      )
+
+      game.add_pieces_to_board
+
+      white_pieces = game.pieces.where(player_color: game.white_player)
+      black_pieces = game.pieces.where(player_color: game.black_player)
+      expect(white_pieces.count).to eq(16)
+      expect(black_pieces.count).to eq(0)
+
+      black_player = FactoryBot.create(:player, playername: 'John')
+      game.black_player = black_player
+
+      game.add_pieces_to_board
+
+      white_pieces = game.pieces.where(player_color: game.white_player)
+      black_pieces = game.pieces.where(player_color: game.black_player)
       expect(white_pieces.count).to eq(16)
       expect(black_pieces.count).to eq(16)
     end
   end
-
 end
