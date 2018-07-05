ba735baacafc236ea763f9b02779ae5ec35f70db
KD33 <Walshk94@gmail.com>
2018-07-05 03:34:39 +0000

    Implemented and added test for bishop movement

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,5 +1,6 @@
 class Bishop < Piece
   def valid_move?(new_row, new_col)
+    return false unless (new_row - self.row).abs == (new_col - self.column).abs
     super &&
     valid_movement?(7, -7, new_row - self.row) &&
     valid_movement?(7, -7, new_col - self.column)
--- spec/models/bishop_spec.rb
+++ spec/models/bishop_spec.rb
@@ -4,12 +4,21 @@ RSpec.describe Bishop, :type => :model do
   describe "a player wants to move his bishop" do
     it "does not allow the bishop to move in only 1 direction" do
       bishop = FactoryBot.build(:bishop)
+      #move column only
       bishop.move(1,5)
       expect(bishop.row).to eq(1)
       expect(bishop.column).to eq(7)
+      #move rows only
+      bishop.move(3,7)
+      expect(bishop.row).to eq(1)
+      expect(bishop.column).to eq(7)
     end
+    it "allows the bishop to move the same amount of rows and same amount of columns" do
+      bishop = FactoryBot.build(:bishop)
+      bishop.move(2,6)
+      expect(bishop.row).to eq(2)
+      expect(bishop.column).to eq(6)
+    end
+
   end
 end
-
-
-#fix this. lets the piece move in one direction
