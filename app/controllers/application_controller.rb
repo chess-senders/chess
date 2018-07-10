800b70d212fea702f417bc81f14dd7d61e2a8a97
Davey Wright <daveywright@Daveys-MBP.home>
2018-07-10 11:26:53 +0100

    connect draw pieces with add pieces to render board

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def after_sign_in_path_for(player)
-   redirect_to player_path(player)
+  def after_sign_in_path_for(resource)
+    player_path(resource)
   end
-  
 end
