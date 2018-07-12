371e979e4864ad55ed3859f09f5aff830519b50a
adrianschung <adrianschung@gmail.com>
2018-07-12 09:20:56 -0400

    Adds determining check logic

--- app/models/game.rb
+++ app/models/game.rb
@@ -19,4 +19,13 @@ class Game < ApplicationRecord
   def square_occupied?(column, row)
     pieces.where(column: column, row: row).any?
   end
+
+  def check?(color)
+    king = pieces.where(player_color: color, type: 'King').first
+    opponent_pieces = pieces.where.not(player_color: color)
+    opponent_pieces.each do |piece|
+      return true if piece.valid_move?(king.row, king.column)
+    end
+    false
+  end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,4 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
+  white_player = FactoryBot.create(:player)
+  black_player = FactoryBot.create(:player)
+  game = FactoryBot.create(:game, state: 1, white_player: white_player, black_player: black_player)
+  FactoryBot.create(:piece, player_color: white_player.id, row: 1, column: 1, type: 'King',
+                            game: game)
+  FactoryBot.create(:piece, player_color: black_player.id, row: 1, column: 2, type: 'King',
+                            game: game)
+
+  describe 'white players turn' do
+    it 'will determine if king is in check' do
+      expect(game.check?(white_player)).to eq(true)
+    end
+
+    it 'will not call check if king is not in check' do
+      black_piece.destroy
+      expect(game.check?(white_player)).to eq(false)
+    end
+  end
 end
