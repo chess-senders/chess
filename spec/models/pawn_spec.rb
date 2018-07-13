64904d85b0960eb90cf5b02c4575c0399e775067
tallKitties <bkautt73@gmail.com>
2018-07-13 01:58:19 +0000

    checking for capture & refactored pawn

--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Pawn, type: :model do
-  describe 'a pawn tries to move' do
+  describe 'tries to move' do
     context 'in a valid direction' do
       it 'one row forward for white player' do
         w_player = FactoryBot.create(:player, playername: 'Wayne')
@@ -41,6 +41,34 @@ RSpec.describe Pawn, type: :model do
         pawn.move(5, 1)
         expect(pawn.row).to eq(5)
         expect(pawn.column).to eq(1)
+      end
+
+      it 'to capture a piece' do
+        w_player = FactoryBot.create(:player, playername: 'Wayne')
+        b_player = FactoryBot.create(:player, playername: 'John')
+        game = FactoryBot.create(
+          :game,
+          white_player: w_player,
+          black_player: b_player
+        )
+
+        pawn_to_move = FactoryBot.create(
+          :pawn,
+          game: game,
+          player_color: b_player.id
+        )
+
+        pawn_to_capture = FactoryBot.create(
+          :pawn,
+          game: game,
+          row: 6,
+          column: 2,
+          player_color: w_player.id
+        )
+
+        pawn_to_move.move(6, 2)
+        expect(pawn_to_move.row).to eq(6)
+        expect(pawn_to_move.column).to eq(2)
       end
     end
 
