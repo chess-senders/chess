cd08f204ecc21f57c443fe90476cd3e931e9bd1a
tallKitties <bkautt73@gmail.com>
2018-07-13 00:26:36 +0000

    white player forward movement

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -9,7 +9,11 @@ class Pawn < Piece
 
   def valid_move?(new_row, new_col)
     super &&
-      valid_movement?(1, 1, (new_row - row).abs) &&
-      valid_movement?(0, 0, new_col - column)
+      valid_movement?(0, 0, new_col - column) &&
+
+      # forward movement only
+      if game.white_player_id == player_color
+        valid_movement?(1, 1, new_row - row)
+      end
   end
 end
