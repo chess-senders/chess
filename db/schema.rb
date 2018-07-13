6d21662e7c0300b79208343defb6b756b3857781
tallKitties <bkautt73@gmail.com>
2018-07-13 02:14:31 +0000

    added moved column (bool) to pieces, set to true on piece's first move

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180710124720) do
+ActiveRecord::Schema.define(version: 20180713021002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20180710124720) do
     t.string   "type"
     t.string   "picture"
     t.integer  "player_id"
+    t.boolean  "moved",        default: false
     t.index ["game_id"], name: "index_pieces_on_game_id", using: :btree
     t.index ["player_id"], name: "index_pieces_on_player_id", using: :btree
   end
