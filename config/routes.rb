c48331a1e9969445fbd6b8df25eafef4bed2bc3a
Davey Wright <daveywright@Daveys-MBP.home>
2018-07-17 18:17:55 +0100

    feat: add ajax to piece move

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :players, controllers: { omniauth_callbacks: 'players/omniauth_callbacks' }
   root 'static_pages#index'
   resources :games
-  resources :pieces
+  resources :pieces, only: :update
   resources :players, only: :show
 end
