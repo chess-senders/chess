16de5900bfba0d397a577ab03178ab2db935bba4
Davey Wright <daveywright@Daveys-MBP.home>
2018-07-18 10:56:19 +0100

    bugfix: bug in rook piece

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -7,9 +7,9 @@ class Rook < Piece
   end
 
   def valid_move?(new_square)
+    return false unless new_square[:row] == row || new_square[:column] == column
     super &&
       valid_movement?(7, -7, new_square[:row] - row) &&
       valid_movement?(7, -7, new_square[:column] - column)
-    return true if new_square[:row] == row || new_square[:column] == column
   end
 end
