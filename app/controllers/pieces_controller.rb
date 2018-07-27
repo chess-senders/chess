3c33bd25f7324629b20c39d7697707d66e38a873
adrianschung <adrianschung@gmail.com>
2018-07-27 09:52:50 -0400

    Adds test for notification

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,11 +1,10 @@
 class PiecesController < ApplicationController
   def update
-    @piece = current_piece
     # stops opponent from moving current players pieces, keep commented for local testing.
       # unless current_player == @piece.player
       #   return redirect_to game_path(game)
       # end
-    Games::UpdateState.call(game) if @piece.move_to!(new_square_params)
+    Games::UpdateState.call(game) if current_piece.move_to!(new_square_params)
     flash[:notice] = '#{game.white_player.playername} is in check' if game.check?(game.white_player)
     flash[:notice] = '#{game.black_player.playername} is in check' if game.check?(game.black_player)
     redirect_to game_path(game)
@@ -19,7 +18,7 @@ class PiecesController < ApplicationController
   end
 
   def game
-    @piece.game
+    @game = current_piece.game
   end
 
   def current_piece
